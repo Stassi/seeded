ea492c714d1d02a384300aa07d32a0d4d521b463
Andreas Stassivik <andreas@stassi.net>
2021-06-01 23:33:10 -0700

    identity permutation module created

---
+++ src/identityPermutation.test.ts
@@ -0,0 +1,7 @@
+import identityPermutation from './identityPermutation'
+
+describe('identityPermutation', () => {
+  test('"length: 4" should output [0, 1, 2, 3]', () => {
+    expect(identityPermutation(4)).toStrictEqual([0, 1, 2, 3])
+  })
+})
---
+++ src/identityPermutation.ts
@@ -0,0 +1,4 @@
+export default function identityPermutation(length: number): number[] {
+  // @ts-ignore
+  return [...Array(length).keys()]
+}
