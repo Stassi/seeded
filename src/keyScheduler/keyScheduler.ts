f5ad19636e10d172833fe6dc2754e41e86a8a7af
Andreas Stassivik <andreas@stassi.net>
2021-06-05 16:15:12 -0700

    forEach replaces variable declaration

--- src/keyScheduler/keyScheduler.ts
+++ src/keyScheduler/keyScheduler.ts
@@ -15,20 +15,20 @@ export default function keyScheduler({
   key,
   keyStreamWidth,
 }: KeySchedulerInput): KeyStream {
-  let i: number = 0
   let j: SwapPointer = swapPointer({ width: keyStreamWidth })
   let keyStream: KeyStream = identityPermutation(keyStreamWidth)
+  const keyStreamOriginal: KeyStream = [...keyStream]
   const remainderKeyLength = remainder(length(key))
   const atKeyIndex = atIndex(key)
   const atKeyStreamIndex = atIndex(keyStream)
 
-  for (; i < keyStreamWidth; i++) {
+  keyStreamOriginal.forEach((i: number): void => {
     j = j.create(
       j.addTo(atKeyIndex(remainderKeyLength(i)), atKeyStreamIndex(i))
     )
     keyStream[j.state] = i
     keyStream[i] = j.state
-  }
+  })
 
   return keyStream
 }
