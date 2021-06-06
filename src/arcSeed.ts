002578d7ca6b7e98aa58d5745059f6d23a52f2b4
Andreas Stassivik <andreas@stassi.net>
2021-06-06 16:59:22 -0700

    parameterize key scheduler components

--- src/arcSeed.ts
+++ src/arcSeed.ts
@@ -1,12 +1,24 @@
+import key from './keyScheduler/key'
 import keyScheduler from './keyScheduler/keyScheduler'
+import pool from './keyScheduler/pool'
+import swapPointer from './keyScheduler/swapPointer'
 
-export default function arcSeed(): any {
+const defaultWidth = 256
+
+interface ArcSeedInput {
+  width?: number
+}
+
+export default function arcSeed(
+  { width = defaultWidth }: ArcSeedInput = { width: defaultWidth }
+) {
   const keyScheduled = keyScheduler({
-    key: [1, 2, 3, 4, 5, 6, 7, 8],
-    width: 256,
+    key: key([1, 2, 3, 4, 5, 6, 7, 8]),
+    pool: pool({ width }),
+    roundKey: swapPointer({ width }),
   })
 
-  console.log({ keyScheduled })
+  console.log(keyScheduled.state)
 
-  return keyScheduled
+  return keyScheduled.state
 }
--- src/keyScheduler/keyScheduler.ts
+++ src/keyScheduler/keyScheduler.ts
@@ -1,24 +1,22 @@
-import key, { Key } from './key'
-import pool, { Pool } from './pool'
-import swapPointer, { SwapPointer } from './swapPointer'
+import { Key } from './key'
+import { Pool } from './pool'
+import { SwapPointer } from './swapPointer'
 
 interface KeySchedulerInput {
-  key: number[]
-  width: number
+  key: Key
+  pool: Pool
+  roundKey: SwapPointer
 }
 
 export default function keyScheduler({
-  width,
-  key: keyParam,
-}: KeySchedulerInput): number[] {
-  const k: Key = key(keyParam)
-  let j: SwapPointer = swapPointer({ width })
-  let s: Pool = pool({ width })
-
-  s.forEach((i: number): void => {
-    j = j.create(j.addTo(k.atIndex(i), s.atIndex(i)))
-    s = s.create(s.swapIndices(i, j.state))
+  key,
+  pool,
+  roundKey,
+}: KeySchedulerInput): KeySchedulerInput['pool'] {
+  pool.forEach((i: number): void => {
+    roundKey = roundKey.create(roundKey.addTo(key.atIndex(i), pool.atIndex(i)))
+    pool = pool.create(pool.swapIndices(i, roundKey.state))
   })
 
-  return s.state
+  return pool
 }
