ac1be8193ec40a4d67949d81ba66acd594c6479e
Andreas Stassivik <andreas@stassi.net>
2021-06-08 01:38:42 -0700

    string added to key input

--- src/keyScheduler/key.ts
+++ src/keyScheduler/key.ts
@@ -1,14 +1,19 @@
 import atIndexUtil from '../utilities/atIndex'
 import length from '../utilities/length'
 import remainder from '../utilities/remainder'
+import toCharCodes from '../utilities/toCharCodes'
 
 export interface Key {
   atIndex: (n: number) => number
 }
 
-export default function key(k: number[]): Key {
-  const atOverflowableIndex = atIndexUtil(k)
-  const remainderLength = remainder(length(k))
+type KeyInput = number[] | string
+
+export default function key(seed: KeyInput): Key {
+  if (typeof seed === 'string') seed = toCharCodes(seed)
+
+  const atOverflowableIndex: (i: number) => number = atIndexUtil(seed),
+    remainderLength: (dividend: number) => number = remainder(length(seed))
 
   function atIndex(n: number): number {
     return atOverflowableIndex(remainderLength(n))
