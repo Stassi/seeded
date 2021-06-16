3f25ed89e35ecb4458734ffa335e5f3c8e988983
Andreas Stassivik <andreas@stassi.net>
2021-06-16 02:10:58 -0700

    expand arcSeed test

--- src/arcSeed.test.ts
+++ src/arcSeed.test.ts
@@ -1,10 +1,20 @@
 import arcSeed, { ArcSeed, KeyStream } from './arcSeed'
 
 describe('arcSeed', () => {
-  const seed = 'hello.'
-  test('it needs a name', () => {
-    const { keyStream }: ArcSeed = arcSeed({ seed })
-    const { key }: KeyStream = keyStream(5)
-    expect(key).toEqual([84, 98, 80, 93, 113])
+  const seed: string = 'hello.',
+    knownKey: number[] = [84, 98, 80, 93, 113]
+
+  describe(`seed (deterministic): "${seed}"`, () => {
+    describe('#keyStream', () => {
+      const keyWidth: number = 5,
+        { keyStream }: ArcSeed = arcSeed({ seed }),
+        { key }: KeyStream = keyStream(keyWidth)
+
+      describe(`keyWidth: ${keyWidth}`, () => {
+        test(`it should return a known, ${keyWidth}-length key: [${knownKey}]`, () => {
+          expect(key).toEqual(knownKey)
+        })
+      })
+    })
   })
 })
