af3e137e524e0ed3a1e7044141bfc4de93b75b74
Andreas Stassivik <andreas@stassi.net>
2021-06-26 00:52:23 -0700

    formatting

--- src/utilities/forEach.ts
+++ src/utilities/forEach.ts
@@ -2,11 +2,7 @@ export interface ForEachProperty {
   forEach: (fn: ForEachCallback) => void
 }
 
-export type ForEachCallback = (
-  value: any,
-  index: number,
-  array: any[]
-) => void
+export type ForEachCallback = (value: any, index: number, array: any[]) => void
 
 export default function forEach(a: any[]) {
   return (fn: ForEachCallback): void => a.forEach(fn)
