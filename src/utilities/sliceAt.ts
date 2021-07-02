b58b96c1672d699c8ac7010417511b031f2b3df0
Andreas Stassivik <andreas@stassi.net>
2021-07-02 13:40:33 -0700

    array slicing

--- src/utilities/sliceAt.ts
+++ src/utilities/sliceAt.ts
@@ -1,5 +1,7 @@
-export type SliceAtCallback = (s: string) => string
+type ArrayOrString = any[] | string
+
+export type SliceAtCallback = (x: ArrayOrString) => any
 
 export default function sliceAt(n: number): SliceAtCallback {
-  return (s: string) => s.slice(n)
+  return (x: ArrayOrString) => x.slice(n)
 }
