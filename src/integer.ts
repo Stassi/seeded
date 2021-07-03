26a06ed14ce7e1dbe83f5a7bbbfd4bf3c48be6a6
Andreas Stassivik <andreas@stassi.net>
2021-07-03 00:59:45 -0700

    types, signatures implemented

--- src/integer.ts
+++ src/integer.ts
@@ -1,7 +1,26 @@
-interface IntegerInput {}
+import type { Octet, OctetInput } from './octet'
 
-export type Integer = {}
+interface IntegerInput extends OctetInput {}
+
+export interface Integer extends Octet {
+  next: (count?: number) => Integer
+}
 
 export default function integer({ ...props }: IntegerInput = {}): Integer {
-  return { ...props }
+  let generated: Integer['generated'] = [],
+    state: Integer['state'] = {
+      i: 0,
+      pool: [],
+      roundKey: 0,
+    }
+
+  function next(nextCount = 1): Integer {
+    return integer({
+      state,
+      count: nextCount,
+      drop: 0,
+    })
+  }
+
+  return { generated, next, state }
 }
