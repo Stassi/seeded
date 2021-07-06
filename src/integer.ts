460f46adc306f4ad66f44f42a101eaea74f7eb1e
Andreas Stassivik <andreas@stassi.net>
2021-07-06 15:41:55 -0700

    implement integer generator (low, fixed)

--- src/integer.ts
+++ src/integer.ts
@@ -1,21 +1,53 @@
 import type { Octet, OctetInput } from './octet'
+import type { RemainderCallback } from './utilities/remainder'
+import isStrictZero from './utilities/isStrictZero'
+import length from './utilities/length'
+import octet from './octet'
+import remainder from './utilities/remainder'
 
-interface IntegerInput extends OctetInput {}
+interface IntegerInput extends OctetInput {
+  max?: number
+  min?: number
+}
 
 export interface Integer extends Octet {
   next: (count?: number) => Integer
 }
 
-export default function integer({ ...props }: IntegerInput = {}): Integer {
+export default function integer({
+  count = 1,
+  max = 10,
+  min = 0,
+  ...props
+}: IntegerInput = {}): Integer {
+  const remainderMax: RemainderCallback = remainder(max)
+
   let generated: Integer['generated'] = [],
+    localNextOctet: Octet['next'] = () => octet(),
     state: Integer['state'] = {
       i: 0,
       pool: [],
       roundKey: 0,
     }
 
+  while (length(generated) < count) {
+    const {
+      generated: generatedOctet,
+      next: nextOctet,
+      state: octetState,
+    }: Octet = isStrictZero(length(generated))
+      ? octet({ ...props })
+      : localNextOctet()
+
+    generated = [...generated, ...generatedOctet.map(remainderMax)]
+    localNextOctet = nextOctet
+    state = octetState
+  }
+
   function next(nextCount = 1): Integer {
     return integer({
+      max,
+      min,
       state,
       count: nextCount,
       drop: 0,
