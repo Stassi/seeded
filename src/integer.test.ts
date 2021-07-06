ae90f10b68cad100cb1c5a999d5e27a0a835432b
Andreas Stassivik <andreas@stassi.net>
2021-07-06 16:04:18 -0700

    maximums test range extended

--- src/integer.test.ts
+++ src/integer.test.ts
@@ -1,58 +1,184 @@
 import type { Integer } from './integer'
 import delay from './utilities/delay'
 import integer from './integer'
+import { bitsInOctet, poolWidth } from './integers.json'
 
 describe('integer', () => {
   describe(`deterministic`, () => {
-    const seed: string = 'hello.',
-      keyWidth: number = 5,
-      doubleKeyWidth: number = keyWidth * 2,
-      knownIntegers: number[] = [3, 4, 4, 2, 8],
-      nextKnownIntegers: number[] = [9, 9, 6, 0, 1],
-      compositeIntegers: number[] = [...knownIntegers, ...nextKnownIntegers]
+    describe(`max: ${bitsInOctet}`, () => {
+      const max: number = bitsInOctet,
+        seed: string = 'hello.',
+        keyWidth: number = 5,
+        doubleKeyWidth: number = keyWidth * 2,
+        knownIntegers: number[] = [1, 6, 6, 4, 6],
+        nextKnownIntegers: number[] = [7, 1, 4, 0, 5],
+        compositeIntegers: number[] = [...knownIntegers, ...nextKnownIntegers]
 
-    describe('first chained call', () => {
-      const { generated, next: nextInteger }: Integer = integer({
-        seed,
-        count: keyWidth,
-      })
+      describe('first chained call', () => {
+        const { generated, next: nextInteger }: Integer = integer({
+          max,
+          seed,
+          count: keyWidth,
+        })
 
-      test('it should persistently return known integers', () => {
-        expect(generated).toEqual(knownIntegers)
-      })
-
-      describe('second chained call', () => {
         test('it should persistently return known integers', () => {
-          const { generated: generatedTwo }: Integer = nextInteger(keyWidth)
-          expect(generatedTwo).toEqual(nextKnownIntegers)
+          expect(generated).toEqual(knownIntegers)
+        })
+
+        describe('second chained call', () => {
+          test('it should persistently return known integers', () => {
+            const { generated: generatedTwo }: Integer = nextInteger(keyWidth)
+            expect(generatedTwo).toEqual(nextKnownIntegers)
+          })
+        })
+      })
+
+      describe('composite call', () => {
+        test('it should persistently return known integers', () => {
+          const { generated }: Integer = integer({
+            max,
+            seed,
+            count: doubleKeyWidth,
+          })
+
+          expect(generated).toEqual(compositeIntegers)
+        })
+      })
+
+      describe('state loading', () => {
+        const { state }: Integer = integer({
+            max,
+            seed,
+            count: keyWidth,
+          }),
+          { generated }: Integer = integer({
+            max,
+            state,
+            count: keyWidth,
+            drop: 0,
+          })
+
+        test('it should return known integers from a loaded state', () => {
+          expect(generated).toEqual(nextKnownIntegers)
         })
       })
     })
 
-    describe('composite call', () => {
-      test('it should persistently return known integers', () => {
-        const { generated }: Integer = integer({
+    describe(`max: ${poolWidth}`, () => {
+      const max: number = poolWidth,
+        seed: string = 'hello.',
+        keyWidth: number = 5,
+        doubleKeyWidth: number = keyWidth * 2,
+        knownIntegers: number[] = [113, 134, 94, 12, 198],
+        nextKnownIntegers: number[] = [119, 249, 116, 160, 21],
+        compositeIntegers: number[] = [...knownIntegers, ...nextKnownIntegers]
+
+      describe('first chained call', () => {
+        const { generated, next: nextInteger }: Integer = integer({
+          max,
           seed,
-          count: doubleKeyWidth,
+          count: keyWidth,
         })
 
-        expect(generated).toEqual(compositeIntegers)
+        test('it should persistently return known integers', () => {
+          expect(generated).toEqual(knownIntegers)
+        })
+
+        describe('second chained call', () => {
+          test('it should persistently return known integers', () => {
+            const { generated: generatedTwo }: Integer = nextInteger(keyWidth)
+            expect(generatedTwo).toEqual(nextKnownIntegers)
+          })
+        })
+      })
+
+      describe('composite call', () => {
+        test('it should persistently return known integers', () => {
+          const { generated }: Integer = integer({
+            max,
+            seed,
+            count: doubleKeyWidth,
+          })
+
+          expect(generated).toEqual(compositeIntegers)
+        })
+      })
+
+      describe('state loading', () => {
+        const { state }: Integer = integer({
+            max,
+            seed,
+            count: keyWidth,
+          }),
+          { generated }: Integer = integer({
+            max,
+            state,
+            count: keyWidth,
+            drop: 0,
+          })
+
+        test('it should return known integers from a loaded state', () => {
+          expect(generated).toEqual(nextKnownIntegers)
+        })
       })
     })
 
-    describe('state loading', () => {
-      const { state }: Integer = integer({
+    describe('max: 1000', () => {
+      const max: number = 1000,
+        seed: string = 'hello.',
+        keyWidth: number = 5,
+        doubleKeyWidth: number = keyWidth * 2,
+        knownIntegers: number[] = [0, 1, 2, 3, 4],
+        nextKnownIntegers: number[] = [5, 6, 7, 8, 9],
+        compositeIntegers: number[] = [...knownIntegers, ...nextKnownIntegers]
+
+      describe('first chained call', () => {
+        const { generated, next: nextInteger }: Integer = integer({
+          max,
           seed,
           count: keyWidth,
-        }),
-        { generated }: Integer = integer({
-          state,
-          count: keyWidth,
-          drop: 0,
         })
 
-      test('it should return known integers from a loaded state', () => {
-        expect(generated).toEqual(nextKnownIntegers)
+        test('it should persistently return known integers', () => {
+          expect(generated).toEqual(knownIntegers)
+        })
+
+        describe('second chained call', () => {
+          test('it should persistently return known integers', () => {
+            const { generated: generatedTwo }: Integer = nextInteger(keyWidth)
+            expect(generatedTwo).toEqual(nextKnownIntegers)
+          })
+        })
+      })
+
+      describe('composite call', () => {
+        test('it should persistently return known integers', () => {
+          const { generated }: Integer = integer({
+            max,
+            seed,
+            count: doubleKeyWidth,
+          })
+
+          expect(generated).toEqual(compositeIntegers)
+        })
+      })
+
+      describe('state loading', () => {
+        const { state }: Integer = integer({
+            max,
+            seed,
+            count: keyWidth,
+          }),
+          { generated }: Integer = integer({
+            max,
+            state,
+            count: keyWidth,
+            drop: 0,
+          })
+
+        test('it should return known integers from a loaded state', () => {
+          expect(generated).toEqual(nextKnownIntegers)
+        })
       })
     })
   })
