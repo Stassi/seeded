909b77f00741d5f31a58a9e757ee9442d7e66f0f
Andreas Stassivik <andreas@stassi.net>
2021-07-08 15:20:51 -0700

    integer named export

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+import type { Integer } from './integer'
 import type { Interval } from './interval'
 import type { Octet } from './octet'
+import integer from './integer'
 import interval from './interval'
 import octet from './octet'
 
-export type { Interval, Octet }
-export { interval, octet }
+export type { Integer, Interval, Octet }
+export { integer, interval, octet }
