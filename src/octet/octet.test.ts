e67802571d067a9ff0d4938641dc5205d68425d5
Andreas Stassivik <andreas@stassi.net>
2021-07-08 21:38:57 -0700

    test explicit & default ranges [0, 256)

--- src/octet/octet.test.ts
+++ src/octet/octet.test.ts
@@ -1,81 +1,118 @@
 import type { Octet } from './octet'
 import delayTen from '../utilities/delayTen'
+import length from '../utilities/length'
 import octet from './octet'
+import { poolWidth } from '../integers.json'
 
 describe('octet', () => {
-  describe('deterministic', () => {
-    const seed: string = 'hello.',
-      count: number = 5,
-      doubleCount: number = count * 2,
-      knownOctets: number[] = [113, 134, 94, 12, 198],
-      nextKnownOctets: number[] = [119, 249, 116, 160, 21],
-      compositeOctets: number[] = [...knownOctets, ...nextKnownOctets]
+  describe.each([
+    {
+      expected: [
+        [113, 134, 94, 12, 198],
+        [119, 249, 116, 160, 21],
+      ],
+      max: undefined,
+      min: undefined,
+    },
+    {
+      expected: [
+        [113, 134, 94, 12, 198],
+        [119, 249, 116, 160, 21],
+      ],
+      max: 256,
+      min: 0,
+    },
+  ])(
+    'range: [$min, $max)',
+    ({
+      max,
+      min,
+      expected: [firstExpected, secondExpected],
+    }: {
+      expected: number[][]
+      max?: number
+      min?: number
+    }) => {
+      describe(`deterministic`, () => {
+        const seed: string = 'hello.',
+          expected: number[] = [...firstExpected, ...secondExpected],
+          firstExpectedLength: number = length(firstExpected),
+          expectedLength: number = length(expected)
 
-    describe('first chained call', () => {
-      const { generated, next: nextOctet }: Octet = octet({ count, seed })
+        describe('first chained call', () => {
+          const { generated, next: nextOctet }: Octet = octet({
+            max,
+            min,
+            seed,
+            count: firstExpectedLength,
+          })
 
-      test('it should persistently return known octets', () => {
-        expect(generated).toEqual(knownOctets)
-      })
+          it('should persistently return known octets', () => {
+            expect(generated).toEqual(firstExpected)
+          })
 
-      describe('second chained call', () => {
-        test('it should persistently return known octets', () => {
-          const { generated: generatedTwo }: Octet = nextOctet(count)
-          expect(generatedTwo).toEqual(nextKnownOctets)
+          describe('second chained call', () => {
+            it('should persistently return known octets', () => {
+              const { generated: generatedTwo }: Octet =
+                nextOctet(firstExpectedLength)
+              expect(generatedTwo).toEqual(secondExpected)
+            })
+          })
+        })
+
+        describe('composite call', () => {
+          it('should persistently return known octets', () => {
+            const { generated }: Octet = octet({
+              max,
+              min,
+              seed,
+              count: expectedLength,
+            })
+
+            expect(generated).toEqual(expected)
+          })
+        })
+
+        describe('state loading', () => {
+          const { state }: Octet = octet({
+              max,
+              min,
+              seed,
+              count: firstExpectedLength,
+            }),
+            { generated }: Octet = octet({
+              max,
+              min,
+              state,
+              count: firstExpectedLength,
+              drop: 0,
+            })
+
+          it('should return known octets from a loaded state', () => {
+            expect(generated).toEqual(secondExpected)
+          })
         })
       })
-    })
 
-    describe('composite call', () => {
-      test('it should persistently return known octets', () => {
-        const { generated }: Octet = octet({ seed, count: doubleCount })
+      describe(`stochastic`, () => {
+        const stochasticPair = async (): Promise<[number, number]> => {
+          const single = (): number => octet({ max, min }).generated[0]
+          const x: number = single()
+          await delayTen()
+          const y: number = single()
+          return [x, y]
+        }
 
-        expect(generated).toEqual(compositeOctets)
+        describe('multiple instances', () => {
+          it('should return discrete values within specified range', async () => {
+            const [x, y]: [number, number] = await stochasticPair()
+            expect(x).toBeGreaterThanOrEqual(min || 0)
+            expect(x).toBeLessThanOrEqual(max || poolWidth - 1)
+            expect(y).toBeGreaterThanOrEqual(min || 0)
+            expect(y).toBeLessThanOrEqual(max || poolWidth - 1)
+          })
+        })
       })
-    })
-
-    describe('state loading', () => {
-      const { state }: Octet = octet({ count, seed }),
-        { generated }: Octet = octet({ count, state, drop: 0 })
-
-      test('it should return known octets from a loaded state', () => {
-        expect(generated).toEqual(nextKnownOctets)
-      })
-    })
-  })
-
-  describe('stochastic', () => {
-    describe('multiple instances', () => {
-      test('it should return distinct values', async () => {
-        const {
-          generated: [x],
-        } = octet()
-
-        await delayTen()
-
-        const {
-          generated: [y],
-        } = octet()
-
-        expect(x === y).toBeFalsy()
-      })
-
-      test('it should return discrete integers between [0, 255]', async () => {
-        const {
-          generated: [x],
-        } = octet()
-
-        await delayTen()
-
-        const {
-          generated: [y],
-        } = octet()
-
-        expect(x).toBeGreaterThanOrEqual(0)
-        expect(x).toBeLessThanOrEqual(255)
-        expect(y).toBeGreaterThanOrEqual(0)
-        expect(y).toBeLessThanOrEqual(255)
-      })
-    })
-  })
+    }
+  )
 })
