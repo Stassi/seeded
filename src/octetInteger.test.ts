96ef7eaad8395485d0208fb2db72c1c7d6b46194
Andreas Stassivik <andreas@stassi.net>
2021-07-09 14:09:41 -0700

    merge integer into octetInteger test cases

--- src/octetInteger.test.ts
+++ src/octetInteger.test.ts
@@ -1,6 +1,7 @@
 import type { OctetInteger } from './octetInteger'
 import delayTen from './utilities/delayTen'
 import length from './utilities/length'
+import maximumSafeBinary from './utilities/maximumSafeBinary'
 import negate from './utilities/negate'
 import octetInteger from './octetInteger'
 import { rangeUnderflowErrorMsg } from './octet/octet'
@@ -10,11 +11,35 @@ describe('octetInteger', () => {
   describe.each([
     {
       expected: [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+      ],
+      max: 1,
+      min: negate(0.9),
+    },
+    {
+      expected: [
         [1, 0, 0, 0, 0],
         [1, 1, 0, 0, 1],
       ],
       max: 1.9,
       min: negate(0.9),
+    },
+    {
+      expected: [
+        [1, 0, 0, 0, 0],
+        [1, 1, 0, 0, 1],
+      ],
+      max: 2,
+      min: 0,
+    },
+    {
+      expected: [
+        [1, 1, 0, -1, -1],
+        [1, -1, 1, 0, -1],
+      ],
+      max: 2,
+      min: negate(1),
     },
     {
       expected: [
@@ -63,6 +88,48 @@ describe('octetInteger', () => {
       ],
       max: 0,
       min: negate(bitsInOctet),
+    },
+    {
+      expected: [
+        [
+          429213476091897, 1301914878543128, 2786706481541730, 976053428009042,
+          1582399419995796,
+        ],
+        [
+          3099175663437328, 921365178165718, 273022197705261, 2353355207890545,
+          3847802525837347,
+        ],
+      ],
+      max: maximumSafeBinary,
+      min: 0,
+    },
+    {
+      expected: [
+        [
+          -4074386151278599, -3201684748827368, -1716893145828766,
+          -3527546199361454, -2921200207374700,
+        ],
+        [
+          -1404423963933168, -3582234449204778, -4230577429665235,
+          -2150244419479951, -655797101533149,
+        ],
+      ],
+      max: 0,
+      min: negate(maximumSafeBinary),
+    },
+    {
+      expected: [
+        [
+          -3645172675186702, -1899769870284240, 1069813335712964,
+          -2551492771352412, -1338800787378904,
+        ],
+        [
+          1694751699504160, -2660869271039060, -3957555231959974,
+          203110788410594, 3192005424304198,
+        ],
+      ],
+      max: maximumSafeBinary,
+      min: negate(maximumSafeBinary),
     },
   ])(
     'range: [$min, $max)',
