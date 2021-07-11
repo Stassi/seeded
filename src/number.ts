f0a7276c0f895305f11710d267c3a12ec08ee53d
Andreas Stassivik <andreas@stassi.net>
2021-07-11 15:09:07 -0700

    number generator implemented

---
+++ src/number.ts
@@ -0,0 +1,34 @@
+import type { Cipher, CipherInput, CipherInputOptional } from './cipher'
+import integer from './integer'
+import interval from './interval'
+import timeSinceEpoch from './utilities/timeSinceEpoch'
+import { defaultDrop, maximumSafeBinary } from './data'
+
+interface NumberInput extends CipherInputOptional {
+  discrete?: boolean
+}
+
+export default function number({
+  count = 1,
+  discrete = false,
+  drop = defaultDrop,
+  max = discrete ? maximumSafeBinary : 1,
+  min = 0,
+  seed = `${timeSinceEpoch()}`,
+  state = {
+    i: 0,
+    pool: undefined,
+    roundKey: 0,
+  },
+}: NumberInput = {}): Cipher {
+  const props: CipherInput = {
+    count,
+    drop,
+    max,
+    min,
+    seed,
+    state,
+  }
+
+  return discrete ? integer(props) : interval(props)
+}
