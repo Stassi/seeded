8ef0abbd42f3ebd274cf2a5d86c3d6caeb44b11c
Andreas Stassivik <andreas@stassi.net>
2021-07-11 12:23:42 -0700

    number test stub

---
+++ src/number.test.ts
@@ -0,0 +1,39 @@
+describe('number', () => {
+  describe('deterministic', () => {
+    describe('first chained call', () => {
+      it('should have tests', () => {
+        throw new Error('unimplemented')
+      })
+
+      describe('second chained call', () => {
+        it('should have tests', () => {
+          throw new Error('unimplemented')
+        })
+      })
+    })
+
+    describe('composite call', () => {
+      it('should have tests', () => {
+        throw new Error('unimplemented')
+      })
+    })
+
+    describe('state loading', () => {
+      it('should have tests', () => {
+        throw new Error('unimplemented')
+      })
+    })
+  })
+
+  describe('stochastic', () => {
+    it('should have tests', () => {
+      throw new Error('unimplemented')
+    })
+  })
+
+  describe('range underflow errors', () => {
+    it('should have tests', () => {
+      throw new Error('unimplemented')
+    })
+  })
+})
