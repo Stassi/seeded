0758e83896e14d7fced1e0468b9fbc48630d8f49
Andreas Stassivik <andreas@stassi.net>
2021-07-12 00:56:39 -0700

    export only primary interface

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
-import type { Cipher } from './cipher'
-import integer from './integer'
-import interval from './interval'
 import number from './number'
 
-export type { Cipher }
-export { integer, interval, number }
+export { number }
 export default number
