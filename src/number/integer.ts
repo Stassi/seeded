8d2ef382927c15ecc155d37fe0b6c65853184962
Andreas Stassivik <andreas@stassi.net>
2021-07-14 01:19:33 -0700

    flatten integer submodule

--- src/number/integer.ts
+++ src/number/integer.ts
@@ -1,8 +1,8 @@
-import type { Cipher, CipherInput } from '../../cipher'
+import type { Cipher, CipherInput } from '../cipher'
 import largeInteger from './largeInteger'
-import negate from '../../utilities/negate'
+import negate from '../utilities/negate'
 import octet from './octet'
-import { poolWidth } from '../../data'
+import { poolWidth } from '../data'
 
 export default function integer({ max, min, ...props }: CipherInput): Cipher {
   const options = { max, min, ...props },
