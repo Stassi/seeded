3e2ead796424502047f330e606133f7b1c6b964a
Andreas Stassivik <andreas@stassi.net>
2021-07-14 18:46:44 -0700

    PoolState type extraction

--- src/cipher/pool.ts
+++ src/cipher/pool.ts
@@ -1,27 +1,28 @@
 import type { AtIndexProperty } from '../utilities/atIndex'
+import type { CipherInput } from './Cipher'
 import type { ForEachProperty } from '../utilities/forEach'
 import type { SwapIndicesProperty } from '../utilities/swapIndices'
 import atIndexUtil from '../utilities/atIndex'
 import forEachUtil from '../utilities/forEach'
 import swapIndicesUtil from '../utilities/swapIndices'
 
-export type PoolInput = number[]
+type PoolState = CipherInput['state']['pool']
 
 export interface Pool
   extends AtIndexProperty,
     ForEachProperty,
     SwapIndicesProperty {
-  create: (state: number[]) => Pool
-  state: number[]
+  create: (state: PoolState) => Pool
+  state: PoolState
 }
 
-export default function pool(state: PoolInput): Pool {
+export default function pool(state: PoolState): Pool {
   const atIndex: Pool['atIndex'] = atIndexUtil(state),
     forEach: Pool['forEach'] = forEachUtil(state),
     swapIndices: Pool['swapIndices'] = swapIndicesUtil(state)
 
-  function create(state: Pool['state']): Pool {
-    return pool(state)
+  function create(newState: PoolState): Pool {
+    return pool(newState)
   }
 
   return { atIndex, create, forEach, state, swapIndices }
