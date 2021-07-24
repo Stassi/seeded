78ca637def94c157610cd3e9bbf27203fff9f1ea
Andreas Stassivik <andreas@stassi.net>
2021-07-24 01:04:33 -0700

    quick uniform sample implemented
    Signed-off-by: Andreas Stassivik <andreas@stassi.net>

--- src/quickUniformSample.ts
+++ src/quickUniformSample.ts
@@ -1,11 +1,36 @@
 import type { Sample, SampleParams } from './sample'
+import length from './utilities/length'
+import { poolWidth } from './data'
 import sample from './sample'
 
 export type QuickUniformSample<T> = Sample<T>
-export type QuickUniformSampleParams<T> = SampleParams<T>
 
-export default function quickUniformSample<T>(
-  props: QuickUniformSampleParams<T>
-): QuickUniformSample<T> {
-  return sample(props)
+export interface QuickUniformSampleParams<T> {
+  count?: SampleParams<T>['count']
+  distribution: T[]
+  drop?: SampleParams<T>['drop']
+  seed?: SampleParams<T>['seed']
+  state?: SampleParams<T>['state']
+}
+
+export default function quickUniformSample<T>({
+  distribution: prevDistribution,
+  ...props
+}: QuickUniformSampleParams<T>): QuickUniformSample<T> {
+  if (length(prevDistribution) > poolWidth)
+    throw new RangeError(`total weight must not exceed ${poolWidth}`)
+
+  const distribution: SampleParams<T>['distribution'] = prevDistribution.map(
+    (
+      value: QuickUniformSampleParams<T>['distribution'][number]
+    ): {
+      weight: SampleParams<T>['distribution'][number]['weight']
+      value: SampleParams<T>['distribution'][number]['value']
+    } => ({
+      value,
+      weight: 1,
+    })
+  )
+
+  return sample({ ...props, distribution })
 }
