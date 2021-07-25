bf87d2e89529df54eaf9b32826d5c81a77c74ee6
Andreas Stassivik <andreas@stassi.net>
2021-07-24 17:51:08 -0700

    Pool type import
    Signed-off-by: Andreas Stassivik <andreas@stassi.net>

--- src/cipher/pool.ts
+++ src/cipher/pool.ts
@@ -1,20 +1,9 @@
-import type { AtIndexProperty } from '../utilities/atIndex'
-import type { CipherParams } from './Cipher'
-import type { ForEachProperty } from '../utilities/forEach'
-import type { SwapIndicesProperty } from '../utilities/swapIndices'
+import type { Pool } from './Cipher'
 import atIndexUtil from '../utilities/atIndex'
 import forEachUtil from '../utilities/forEach'
 import swapIndicesUtil from '../utilities/swapIndices'
 
-type PoolState = CipherParams['state']['pool']
-
-export interface Pool
-  extends AtIndexProperty,
-    ForEachProperty,
-    SwapIndicesProperty {
-  create: (state: PoolState) => Pool
-  state: PoolState
-}
+type PoolState = Pool['state']
 
 export default function pool(state: PoolState): Pool {
   const atIndex: Pool['atIndex'] = atIndexUtil(state),
