7135f3cf0e4dbc8f11e237e4bf4a173ed66f2ad1
Andreas Stassivik <andreas@stassi.net>
2021-07-24 18:06:10 -0700

    preserve module type exports
    Signed-off-by: Andreas Stassivik <andreas@stassi.net>

--- src/cipher/index.ts
+++ src/cipher/index.ts
@@ -1,22 +1,22 @@
 import type Cipher from './Cipher'
 import type {
-  CipherParamsOptional,
   CipherIntegerOrInterval,
   CipherParams,
+  CipherParamsOptional,
   CipherPersistent,
   CipherRangeUnderflowParams,
+  Pool,
+  RoundKey,
 } from './Cipher'
-import type { Pool } from './pool'
-import type { RoundKey } from './roundKey'
 import keySchedule from './keySchedule'
 import pool from './pool'
 import roundKey from './roundKey'
 
 export type {
   Cipher,
-  CipherParamsOptional,
   CipherIntegerOrInterval,
   CipherParams,
+  CipherParamsOptional,
   CipherPersistent,
   CipherRangeUnderflowParams,
   Pool,
