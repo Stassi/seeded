e3f7df7233cd70149c0b837054c33e04f7a4b6fc
Andreas Stassivik <andreas@stassi.net>
2021-07-27 21:01:41 -0700

    WeightedValue, SampleParams types export
    Signed-off-by: Andreas Stassivik <andreas@stassi.net>

--- src/sample/Samples.ts
+++ src/sample/Samples.ts
@@ -1,14 +1,21 @@
 import type { NumberParams } from '../number'
 
+export interface WeightedValue<T> {
+  value: T
+  weight: number
+}
+
 export interface SampleWeightedParams<T> extends Partial<NumberParams> {
-  distribution: {
-    value: T
-    weight: number
-  }[]
+  distribution: WeightedValue<T>[]
 }
 
 export interface SampleUniformParams<T> extends Partial<NumberParams> {
   distribution: T[]
+}
+
+export interface SampleParams<T>
+  extends Pick<NumberParams, 'count' | 'drop' | 'seed' | 'state'> {
+  distribution: (T | WeightedValue<T>)[]
 }
 
 export interface Sample<T> {
