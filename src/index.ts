892140a536675eee5f6771d2f47b311d1ff26b48
Andreas Stassivik <andreas@stassi.net>
2021-07-28 06:03:01 -0700

    superfluous named export removal
    Signed-off-by: Andreas Stassivik <andreas@stassi.net>

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import number from './number'
-import sample, { sampleWeighted } from './sample'
+import sample from './sample'
 
-export { number, sample, sampleWeighted }
+export { number, sample }
 export default number
--- src/sample/index.ts
+++ src/sample/index.ts
@@ -1,5 +1,3 @@
 import sample from './sample'
-import sampleWeighted from './sampleWeighted'
 
-export { sampleWeighted }
 export default sample
