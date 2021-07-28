c55c23002a6cca34e5d2027f27964445462c9003
Andreas Stassivik <andreas@stassi.net>
2021-07-28 05:34:06 -0700

    expandedDistribution extraction
    Signed-off-by: Andreas Stassivik <andreas@stassi.net>

--- src/sample/Samples.ts
+++ src/sample/Samples.ts
@@ -1,4 +1,6 @@
 import type { NumberParams } from '../number'
+import isStrictZero from '../utilities/isStrictZero'
+import length from '../utilities/length'
 
 export interface WeightedValue<T> {
   value: T
@@ -25,3 +27,17 @@ export interface Sample<T> {
 }
 
 export type SampleUniform<T> = Pick<Sample<T>, 'generated' | 'state'>
+
+export function expandedDistribution<T>(
+  distribution: SampleParams<T>['distribution']
+): distribution is WeightedValue<T>[] {
+  const distributionKeys: ReturnType<typeof Object.keys> = Object.keys(
+    distribution[0]
+  )
+
+  return isStrictZero(length(distributionKeys))
+    ? false
+    : distributionKeys.every(
+        (key: string) => key === 'value' || key === 'weight'
+      )
+}
