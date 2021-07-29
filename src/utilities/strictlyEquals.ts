9ac0fcd56b4ee9f9f5969c0124449bedc3f938fa
Andreas Stassivik <andreas@stassi.net>
2021-07-29 15:35:51 -0700

    strictlyEquals default export
    Signed-off-by: Andreas Stassivik <andreas@stassi.net>

--- src/utilities/strictlyEquals.ts
+++ src/utilities/strictlyEquals.ts
@@ -2,9 +2,9 @@ import type { Callback } from './Callback'
 
 type IsNumberCallback = Callback<number, boolean>
 
-function strictlyEquals<T>(x: T): Callback<T, boolean> {
-  return (y: T) => x === y
-}
-
 export const strictlyEqualsOne: IsNumberCallback = strictlyEquals(1)
 export const strictlyEqualsZero: IsNumberCallback = strictlyEquals(0)
+
+export default function strictlyEquals<T>(x: T): Callback<T, boolean> {
+  return (y: T) => x === y
+}
