b338936abc2cd8d84e3f9bdd06ee1b5e62e81748
Andreas Stassivik <andreas@stassi.net>
2021-07-30 14:45:32 -0700

    toCharCodes utilities import
    Signed-off-by: Andreas Stassivik <andreas@stassi.net>

--- src/utilities/toCharCodes.ts
+++ src/utilities/toCharCodes.ts
@@ -1,3 +1,13 @@
-export default function toCharCodes(s: string): number[] {
-  return s.split('').map((x: string) => x.charCodeAt(0))
+import type { S } from './strings'
+import map from './map'
+import split from './split'
+
+type MapCallback = ReturnType<typeof map>
+type UTF16 = ReturnType<typeof String.prototype.charCodeAt>
+type UTF16s = UTF16[]
+
+const stringsToUTF16: MapCallback = map((x: S): UTF16 => x.charCodeAt(0))
+
+export default function toCharCodes(s: S): UTF16s {
+  return <UTF16s>stringsToUTF16(split(s))
 }
