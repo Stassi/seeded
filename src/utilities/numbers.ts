226ddfb781f52fc01e2b96648af0dafc1e3c66f0
Andreas Stassivik <andreas@stassi.net>
2021-07-29 23:59:00 -0700

    objects utility implementation
    Signed-off-by: Andreas Stassivik <andreas@stassi.net>

--- src/utilities/numbers.ts
+++ src/utilities/numbers.ts
@@ -1,6 +1,18 @@
 import type { IdentityCallback } from './Callback'
+import type { KeyPrimitives, UniformValues } from './objects'
 
 export type N = number
 export type Ns = N[]
+
 export type NumberCallback = IdentityCallback<N>
 export type NumbersCallback = IdentityCallback<Ns>
+
+export type NumberProperties<Keys extends KeyPrimitives> = UniformValues<
+  Keys,
+  N
+>
+
+export type NumbersProperties<Keys extends KeyPrimitives> = UniformValues<
+  Keys,
+  Ns
+>
