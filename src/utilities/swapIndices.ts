2cff5a9f37a0869ae39f0d23113fe860d676d005
Andreas Stassivik <andreas@stassi.net>
2021-07-30 14:44:27 -0700

    swapIndices generic type parameter
    Signed-off-by: Andreas Stassivik <andreas@stassi.net>

--- src/utilities/swapIndices.ts
+++ src/utilities/swapIndices.ts
@@ -1,16 +1,18 @@
-import atIndexUtil, { AtIndexCallback } from './atIndex'
+import type { Callback } from './Callback'
+import type { N } from './numbers'
+import atIndexUtil from './atIndex'
 
-export interface SwapIndicesProperty {
-  swapIndices: SwapIndicesCallback
+type SwapIndicesCallback<T> = (x: N, y: N) => T[]
+
+export interface SwapIndicesProperty<T> {
+  swapIndices: SwapIndicesCallback<T>
 }
 
-export type SwapIndicesCallback = (x: number, y: number) => any[]
-
-export default function swapIndices(a: any[]): SwapIndicesCallback {
-  return (x: number, y: number): any[] => {
-    const array: any[] = [...a],
-      atIndex: AtIndexCallback = atIndexUtil(array),
-      prevX: any = atIndex(x)
+export default function swapIndices<T>(a: T[]): SwapIndicesCallback<T> {
+  return (x: N, y: N): T[] => {
+    const array: T[] = [...a],
+      atIndex: Callback<N, T> = atIndexUtil(array),
+      prevX: T = atIndex(x)
 
     array[x] = atIndex(y)
     array[y] = prevX
